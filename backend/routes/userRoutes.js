0506efe82f9fccf50b7c1d1ccc3ecf86470bfb6c
Olaoluwa Thompson <137652527+codakyello@users.noreply.github.com>
2025-04-13 18:42:52 +0100

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  generateGuestId,
+  userSignUp,
+  userLogin,
+} = require("../controllers/authController");
+const {
+  getUserCarts,
+  createUserCart,
+  getUserCartSummary,
+} = require("../controllers/userController");
+const router = express.Router();
+
+router.post("/signup", userSignUp);
+
+router.post("/login", userLogin);
+
+router.post("/generateGuestId", generateGuestId);
+
+router.route("/:id/cart").get(getUserCarts).post(createUserCart);
+
+router.route("/:id/cart-summary").get(getUserCartSummary);
+
+module.exports = router;
